072eca12a44185a6c03592ef46598a98558a9bdc
AlexisFarinango <paulpulamarin141220@gmail.com>
2023-12-10 09:03:03 -0500

    asd

--- trabajo1.java
+++ trabajo1.java
@@ -2,6 +2,7 @@ public class trabajo1 {
     public static void main(String[] args){
         System.out.println("Hola mundo");
         System.out.println("asioi mundo");
+        System.out.println("ok");
 
     }
 }
