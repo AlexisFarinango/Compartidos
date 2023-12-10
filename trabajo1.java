90076e3d36f979f5ee8ed81579b173d2967ef75f
AlexisFarinango <133933591+AlexisFarinango@users.noreply.github.com>
2023-12-10 10:04:25 -0500

    trabajo1.java

--- trabajo1.java
+++ trabajo1.java
@@ -1,5 +1,37 @@
 public class trabajo1 {
+    /*Dilan*/
+
+
+
+
+
+    /*Alex*/
+
+
+
+
+
+
+    
+    /*Alexis*/
+    
     public static void main(String[] args){
+        /*Dilan*/
+
+
+
+
+
+        /*Alex*/
+
+
+
+
+
+
+    
+        /*Alexis*/
+    
         System.out.println("Hola mundo");
         System.out.println("asioi mundo");
         System.out.println("ok");
